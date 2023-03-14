fd2c7226282c18f310457b8bdc100fb30331c356
장준성(lkc1641) <lkc1641@ebaykorea.corp>
2023-03-14 09:47:38 +0900

    fix: Modify axios baseUrl

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://192.168.89.63:9090", // for test
+    // baseURL: "http://192.168.89.63:9090", // for test
     // baseURL: "http://localhost:9090",
+    baseURL: "http://newpdfprd2-pdf-admintool-4.d3.clouz.io", // for test
     header:{
         "Content-type":"application/json",
     }
